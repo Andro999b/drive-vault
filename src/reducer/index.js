5ccbc562adb4704f9cfbf2e98062d7108d9794ea
andro <andro999b@gmail.com>
2017-06-03 12:15:31 +0300

    fix create credential dialog

--- src/reducer/index.js
+++ src/reducer/index.js
@@ -35,7 +35,15 @@ export default handleActions({
     'SET_REMOVE_GROUP': (state, action) => ({ ...state, removeGroup: action.payload }),
     'SET_SAVE_GROUP': (state, action) => ({ ...state, saveGroup: action.payload }),
     'SET_REMOVE_CREDENTIAL': (state, action) => ({ ...state, removeCredential: action.payload }),
-    'SET_SAVE_CREDENTIAL': (state, action) => ({ ...state, saveCredential: action.payload }),
+    'SET_SAVE_CREDENTIAL': (state, action) => {
+        const credential = action.payload;
+
+        // set selecetdGroup as credential group by default when create new
+        if(credential != null && credential.groups == null && state.selectedGroup != null)
+            credential.group = state.selectedGroup.id;
+
+        return { ...state, saveCredential: credential };
+    },
     'SET_GROUPS': (state, action) => ({ ...state, groups: action.payload }),
     'SET_CREDENTIALS': (state, action) => ({ ...state, credentials: action.payload }),
     'SET_SELECTED_GROUP': (state, action) => ({ ...state, selectedGroup: action.payload }),
